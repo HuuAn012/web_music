b50c59dc6c41dc993f901ac2e017806d72a82de3
HuuAn012 <nguyenhuuan10122003@gmail.com>
2023-01-06 17:11:32 +0700

    Updates

---
+++ precache-manifest.ed079471bfa44aa1982978252a06644b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "97d8d21a1328ae3e0ef5",
+    "url": "/HuuAn012/web_music.git/static/css/main.0ec7a8aa.chunk.css"
+  },
+  {
+    "revision": "97d8d21a1328ae3e0ef5",
+    "url": "/HuuAn012/web_music.git/static/js/main.97d8d21a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/HuuAn012/web_music.git/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/HuuAn012/web_music.git/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a04a0c7e6caa2f846d11",
+    "url": "/HuuAn012/web_music.git/static/js/runtime~main.a04a0c7e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/HuuAn012/web_music.git/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c1b310d4d960c68eec008de0788216bd",
+    "url": "/HuuAn012/web_music.git/index.html"
+  }
+];
